7ed7a0f79b7cfda72849ee34a529eb72715ab55a
WillForan <willforan@gmail.com>
2019-11-19 12:47:23 -0500

    atlas: Seitzman300

--- atlas_list.m
+++ atlas_list.m
@@ -1,5 +1,5 @@
 function [atlases] = atlas_list(study)
- atlases = { 'GordonHarOx', 'hpc_pfc_brainstem_rstg', 'CogEmoROIs', 'cogemo20181220', 'vmpfcstrvta20181221', 'hpc_apriori_atlas_11','wb1038'};
+ atlases = { 'GordonHarOx', 'hpc_pfc_brainstem_rstg', 'CogEmoROIs', 'cogemo20181220', 'vmpfcstrvta20181221', 'hpc_apriori_atlas_11','wb1038', 'Seitzman300'};
  % before 20190107, in each *rest_table.m as:
  %  cog => {'GordonHarOx', 'hpc_pfc_brainstem_rstg','CogEmoROIs'};
  %  pet => {'GordonHarOx','CogEmoROIs','vmpfcstrvta20181221'};
